e92ed3efa3f6a65c1b46e9c4d0f7bf2e6519a16a
dilshad <mohddilshad3743@gmail.com>
2025-08-23 11:53:23 +0530

    all ok

--- first-web/hook/useProtectRoute.ts
+++ first-web/hook/useProtectRoute.ts
@@ -17,28 +17,7 @@ export function useProtectRoute() {
             return;
         }
 
-        //     fetch("http://localhost:5000/api/auth/verify", {  // ✅ correct endpoint
-        //         method:"POST"
-        //         headers: { "Content-Type": "application/json" },
-        //         body: JSON.stringify({ token }),
-        //     })
-        //         .then(async (res) => {
-        //             const data = await res.json();
-        //             if (res.ok && data.valid) {
-        //                 setUser(data.user);
-        //             } else {
-        //                 sessionStorage.removeItem("token");
-        //                 router.replace("/");
-        //             }
-        //         })
-        //         .catch(() => {
-        //             sessionStorage.removeItem("token");
-        //             router.replace("/");
-        //         })
-        //         .finally(() => setIsChecking(false));
-        // }, [router]);
-
-        axios.post("http://token-implement-backend.vercel.app/api/auth/verify",
+        axios.post("http://token-implement.vercel.app/api/auth/verify",
             { token }, // ✅ send token in body
             { headers: { "Content-Type": "application/json" } } // ✅ headers
         )
